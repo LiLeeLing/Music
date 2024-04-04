20caba2461c34c3c5fc139981ddbcd838cf136ba
LiLeeLing <158540825+LiLeeLing@users.noreply.github.com>
2024-04-04 08:18:57 +0800

    Delete src directory

--- src/contexts/player.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react'
-import { createPlayer } from '@/player/base'
-
-export const player = createPlayer()
-export const PlayerContext = createContext(player)
-export const PlayerProvider = PlayerContext.Provider
--- src/store/bootstrap.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs'
-import localizedFormat from 'dayjs/plugin/localizedFormat'
-import { useUserStore } from './user'
-
-dayjs.extend(localizedFormat)
-
-export default async function () {
-  useUserStore.getState().fetchAccount()
-}
